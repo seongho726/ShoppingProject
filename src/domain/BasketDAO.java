1029cb69b724a485f2a5d9f4d3038981a1473a89
ajdeve <ajdevlife@gmail.com>
2020-12-28 12:05:20 +0900

    BasketDAO
    BasketDAO: minified the code

--- src/domain/BasketDAO.java
+++ src/domain/BasketDAO.java
@@ -1,4 +1,5 @@
 package domain;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,157 +7,84 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import util.DBUtil;
+
 public class BasketDAO {
-    private DBUtil connPool;
-    private static final String RETRIEVE_STMT
-            = "SELECT * FROM shoppingbasket where UserID = ? and Validity = 1";
-    private static final String GETID_STMT = "SELECT COUNT(BasketID) FROM shoppingbasket";
-    private static final String ADD_STMT = "INSERT INTO shoppingbasket VALUES(?,?,?,?,1)";
-    private static final String DELETE_STMT = "UPDATE shoppingbasket SET Validity = 2 WHERE UserID = ? AND BasketID = ?";
-    private static final String CLEAN_STMT = "UPDATE shoppingbasket SET Validity = 2 WHERE UserID = ?";
-    ArrayList<Basket> basketRetrieve(int userid) throws SQLException {
-        ArrayList<Basket> baskets = new ArrayList<Basket>();
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rset = null;
-        try {
-            conn = connPool.getConnection();
-            stmt = conn.prepareStatement(RETRIEVE_STMT);
-            stmt.setInt(1, userid);
-            rset = stmt.executeQuery();
-            while (rset.next()) {
-                int BasketID = rset.getInt(1);
-                int UserID = rset.getInt(2);
-                int ProductID = rset.getInt(3);
-                int Numbers = rset.getInt(4);
-                int Validity = rset.getInt(5);
-                baskets.add(new Basket(BasketID, UserID, ProductID, Numbers, Validity));
-            }
-            return baskets;
-        } catch (SQLException se) {
-            throw new RuntimeException(
-                    "A database error occurred. " + se.getMessage());
-        } catch (Exception e) {
-            throw new RuntimeException("Exception: " + e.getMessage());
-        } finally {
-            if (rset != null) {
-                try {
-                    rset.close();
-                } catch (SQLException se) {
-                    se.printStackTrace(System.err);
-                }
-            }
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException se) {
-                    se.printStackTrace(System.err);
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (Exception e) {
-                    e.printStackTrace(System.err);
-                }
-            }
-        }
-    }
-    void basketAdd(int userId, int productId, int productCount) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rset = null;
-        try {
-            conn = connPool.getConnection();
-            stmt = conn.prepareStatement(GETID_STMT);
-            rset = stmt.executeQuery();
-            int ID = -1;
-            rset.next();
-            ID = rset.getInt("COUNT(BasketID)");
-            ID++;
-            stmt = conn.prepareStatement(ADD_STMT);
-            stmt.setInt(1, ID);
-            stmt.setInt(2, userId);
-            stmt.setInt(3, productId);
-            stmt.setInt(4, productCount);
-            stmt.executeQuery();
-        } catch (SQLException se) {
-            throw new RuntimeException(
-                    "A database error occurred. " + se.getMessage());
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException se) {
-                    se.printStackTrace(System.err);
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (Exception e) {
-                    e.printStackTrace(System.err);
-                }
-            }
-        }
-    }
-    void basketDelete(int userid, int basketid) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rset = null;
-        try {
-            conn = connPool.getConnection();
-            stmt = conn.prepareStatement(DELETE_STMT);
-            stmt.setInt(1, userid);
-            stmt.setInt(2, basketid);
-            stmt.executeQuery();
-        } catch (SQLException se) {
-            throw new RuntimeException(
-                    "A database error occurred. " + se.getMessage());
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException se) {
-                    se.printStackTrace(System.err);
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (Exception e) {
-                    e.printStackTrace(System.err);
-                }
-            }
-        }
-    }
-    void basketClean(int userid) {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rset = null;
-        try {
-            conn = connPool.getConnection();
-            stmt = conn.prepareStatement(CLEAN_STMT);
-            stmt.setInt(1, userid);
-            stmt.executeQuery();
-        } catch (SQLException se) {
-            throw new RuntimeException(
-                    "A database error occurred. " + se.getMessage());
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException se) {
-                    se.printStackTrace(System.err);
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (Exception e) {
-                    e.printStackTrace(System.err);
-                }
-            }
-        }
-    }
+
+	ArrayList<Basket> basketRetrieve(int userId) throws SQLException {
+		ArrayList<Basket> baskets = new ArrayList<Basket>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("SELECT * FROM shoppingbasket where userId = ? and validity = 1");
+			pstmt.setInt(1, userId);
+
+			rset = pstmt.executeQuery();
+			while (rset.next()) {
+				baskets.add(new Basket(rset.getInt(1), rset.getInt(2), (Product) rset.getObject(3),
+						rset.getInt(4), rset.getInt(5)));
+			}
+
+		} finally {
+			DBUtil.close(con, pstmt, rset);
+		}
+		return baskets;
+
+	}
+
+	void basketAdd(int userId, int productId, int productCount) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("SELECT COUNT(basketId) FROM shoppingbasket");
+			rset = pstmt.executeQuery();
+
+			int basketId = -1;
+			rset.next();
+			basketId = rset.getInt("COUNT(basketId)");
+			basketId++;
+
+			pstmt = con.prepareStatement("INSERT INTO shoppingbasket VALUES(?,?,?,?,1)");
+			pstmt.setInt(1, basketId);
+			pstmt.setInt(2, userId);
+			pstmt.setInt(3, productId);
+			pstmt.setInt(4, productCount);
+			pstmt.executeQuery();
+		} finally {
+			DBUtil.close(con, pstmt, rset);
+		}
+	}
+
+	void basketDelete(int userId, int basketId) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("UPDATE shoppingbasket SET validity = 2 WHERE userId = ? AND basketId = ?");
+			pstmt.setInt(1, userId);
+			pstmt.setInt(2, basketId);
+			pstmt.executeQuery();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+	}
+
+	void basketClean(int userId) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("UPDATE shoppingbasket SET Validity = 2 WHERE UserID = ?");
+			pstmt.setInt(1, userId);
+			pstmt.executeQuery();
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+	}
 }
